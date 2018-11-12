487064e5ccc8d81ed986eb9bb185b84b52057f66
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2018-11-12 11:35:06 +0100

    included InvalidDirecotryException

---
+++ src/Exception/InvalidDirectoryException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * kiwi-suite/template (https://github.com/kiwi-suite/template)
+ *
+ * @package kiwi-suite/template
+ * @link https://github.com/kiwi-suite/template
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+declare(strict_types=1);
+
+namespace KiwiSuite\Template\Exception;
+
+class InvalidDirectoryException extends \Exception
+{
+}
